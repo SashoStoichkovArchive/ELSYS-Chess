b93da411a15f0848a7a9a6dca23cf7730278adf5
Alexander Stoichkov <sashostoichkov@gmail.com>
2020-04-25 10:48:54 +0300

    django-channels

--- elsys_chess_app/settings.py
+++ elsys_chess_app/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    # 'active_users.middleware.ActiveUsersSessionMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
