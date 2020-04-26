79d77950b259e96aa86113e5a45b04680b586b0e
Alexander Stoichkov <sashostoichkov@gmail.com>
2020-04-26 16:34:04 +0300

    heroku stuff

--- elsys_chess_app/settings.py
+++ elsys_chess_app/settings.py
@@ -82,7 +82,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('localhost', 6379)],
+            "hosts": [(ALLOWED_HOSTS[0], 6379)],
         },
     },
 }
